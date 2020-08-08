8d312c2fb987b22894f5618ff608d27ce53ce37a
Vivek Baruah <vivekbrh01@gmail.com>
2020-08-08 19:34:51 +0530

    Actor updates added

--- game.js
+++ game.js
@@ -279,7 +279,6 @@ function overlap(actor1, actor2) {
 	);
 }
 
-
 // Collide
 
 Lava.prototype.collide = function (state) {
@@ -291,4 +290,64 @@ Coin.prototype.collide = function (state) {
 	let status = state.status;
 	if (!filtered.some((a) => (a.type = "coin"))) status = "won";
 	return new State(state.level, filtered, status);
+};
+
+// Actor updates
+
+// Lava update
+Lava.prototype.update = function (time, state) {
+	let newPos = this.pos.plus(this.speed.times(time));
+
+	if (!state.level.touches(newPos, this.size, "wall")) {
+		return new Lava(newPos, this.speed, this.reset);
+	} else if (this.reset) {
+		return new Lava(this.reset, this.speed, this.reset);
+	} else {
+		return new Lava(this.pos, this.speed.times(-1));
+	}
+};
+
+// Coin update
+const wobbleSpeed = 8;
+const wobbleDist = 0.07;
+
+Coin.prototype.update = function (time) {
+	let wobble = this.wobble + time * wobbleSpeed;
+	let wobblePos = Math.sin(wobble) * wobbleDist;
+
+	return new Coin(
+		this.basePos.plus(new Vec(0, wobblePos)),
+		this.basePos,
+		wobble
+	);
+};
+
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function (time, state, keys) {
+	let xSpeed = 0;
+
+	if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+	if (keys.ArrowRight) xSpeed += playerXSpeed;
+
+	let pos = this.pos;
+	let movedX = pos.plus(new Vec(xSpeed * time, 0));
+
+	if (!state.level.touches(movedX, this.size, "wall")) {
+		pos = movedX;
+	}
+
+	let ySpeed = this.speed.y + time * gravity;
+	let movedY = pos.plus(new Vec(0, ySpeed * time));
+
+	if (!state.level.touches(movedY, this.size, "wall")) {
+		pos = movedY;
+	} else if (keys.ArrowUp && ySpeed > 0) {
+		ySpeed = -jumpSpeed;
+	} else {
+		ySpeed = 0;
+	}
+	return new Player(pos, new Vec(xSpeed, ySpeed));
 };
